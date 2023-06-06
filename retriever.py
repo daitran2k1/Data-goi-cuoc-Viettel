a5440af4919e13143b4dabc45891d9e42ccd9a65
Tran Quang Dai <tranquangdai7a@gmail.com>
2023-06-06 11:57:34 +0700

    add usage

--- retriever.py
+++ retriever.py
@@ -87,3 +87,38 @@ model = CrossEncoder(model_save_path)
 
 evaluator = CECorrelationEvaluator.from_input_examples(test_samples, name='sts-test')
 evaluator(model)
+
+# from sentence_transformers.cross_encoder import CrossEncoder
+# import numpy as np
+
+# # Pre-trained cross encoder
+# model = CrossEncoder('cross-encoder/stsb-distilroberta-base')
+
+# # We want to compute the similarity between the query sentence
+# query = 'A man is eating pasta.'
+
+# # With all sentences in the corpus
+# corpus = ['A man is eating food.',
+#           'A man is eating a piece of bread.',
+#           'The girl is carrying a baby.',
+#           'A man is riding a horse.',
+#           'A woman is playing violin.',
+#           'Two men pushed carts through the woods.',
+#           'A man is riding a white horse on an enclosed ground.',
+#           'A monkey is playing drums.',
+#           'A cheetah is running behind its prey.'
+#           ]
+
+# # So we create the respective sentence combinations
+# sentence_combinations = [[query, corpus_sentence] for corpus_sentence in corpus]
+
+# # Compute the similarity scores for these combinations
+# similarity_scores = model.predict(sentence_combinations)
+
+# # Sort the scores in decreasing order
+# sim_scores_argsort = reversed(np.argsort(similarity_scores))
+
+# # Print the scores
+# print("Query:", query)
+# for idx in sim_scores_argsort:
+#     print("{:.2f}\t{}".format(similarity_scores[idx], corpus[idx]))
